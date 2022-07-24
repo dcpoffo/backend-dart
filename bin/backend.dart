81c4ff2c7837db33ed4340ee0ed57e901d00d3cd
Darlan <dcpoffo@gmail.com>
2022-07-23 22:27:54 -0300

    Aula 4 - Middleware

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,5 +1,30 @@
-import 'package:backend/backend.dart' as backend;
+import 'dart:async';
 
-void main(List<String> arguments) {
-  print('Hello world: ${backend.calculate()}!');
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+
+void main(List<String> arguments) async {
+  var pipeline = Pipeline().addMiddleware(log());
+
+  final server = await io.serve(pipeline.addHandler(_handler), '0.0.0.0', 4466);
+
+  print('Online - ${server.address.address}: ${server.port}');
+}
+
+Middleware log() {
+  return (handler) {
+    return (request) async {
+      // antes de executar
+      print('ANTES DE EXECUTAR - Solicitado: ${request.url}');
+      var response = await handler(request);
+      // depois de executar
+      print('DEPOIS DE EXECUTAR - [${response.statusCode}] - Response');
+      return response;
+    };
+  };
+}
+
+FutureOr<Response> _handler(Request request) {
+  print(request);
+  return Response(200, body: 'Corpo');
 }
